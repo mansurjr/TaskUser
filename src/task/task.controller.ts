a147039646ae380f010dad2c4bf0d73622ab47c2
mansurjr <ztm007uzb@gmail.com>
2025-07-06 18:56:17 +0500

    Added guards to task

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -10,31 +10,43 @@ import {
   ValidationPipe,
   ParseIntPipe,
   ParseEnumPipe,
+  UseGuards,
 } from "@nestjs/common";
 import { TaskService } from "./task.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { UpdateTaskDto } from "./dto/update-task.dto";
 import { TaskStatus } from "@prisma/client";
+import { Roles } from "../common/decorators/role";
+import { RolesGuard } from "../common/guards/role.guard";
+import { CustomJwtGuard } from "../common/guards/jwt.guard";
 
 @Controller("task")
+@Roles()
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
-
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard, CustomJwtGuard)
   @Post()
   async create(@Body() createTaskDto: CreateTaskDto) {
     return this.taskService.create(createTaskDto);
   }
 
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard, CustomJwtGuard)
   @Get()
   async findAll() {
     return this.taskService.findAll();
   }
 
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard, CustomJwtGuard)
   @Get(":id")
   async findOne(@Param("id", ParseIntPipe) id: number) {
     return this.taskService.findOne(id);
   }
 
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard, CustomJwtGuard)
   @Patch(":id")
   async update(
     @Param("id", ParseIntPipe) id: number,
@@ -43,11 +55,15 @@ export class TaskController {
     return this.taskService.update(id, updateTaskDto);
   }
 
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard, CustomJwtGuard)
   @Delete(":id")
   async remove(@Param("id", ParseIntPipe) id: number) {
     return this.taskService.remove(id);
   }
 
+  @Roles("ADMIN", "USER")
+  @UseGuards(RolesGuard, CustomJwtGuard)
   @Patch(":id/status")
   async updateStatus(
     @Param("id", ParseIntPipe) id: number,
